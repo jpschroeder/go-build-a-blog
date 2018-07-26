e65be8a25c6e7261f07c5ccc786b0baaa1a87d49
John Schroeder <john@schroederspace.com>
2018-07-25 23:38:47 -0400

    accept a parameter to tell the server where to listen

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 	// Accept a command line flag -reset
 	// This flag allows you to change the key needed to edit/delete posts
 	reset := flag.Bool("reset", false, "reset the key used to edit/delete")
+	// Accept a command line flag -addr :8080
+	// This flag tells the server the address to listen on
+	addr := flag.String("addr", ":8080", "the address to listen on (ex localhost:8080)")
+
 	flag.Parse()
 
 	// Connect to the sqlite database and make sure the schema exists
@@ -37,8 +41,8 @@ func main() {
 	http.Handle("/", registerRoutes(db, tmpl))
 
 	// Start the application server
-	log.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Listening on", *addr)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 
 	log.Println("Application Finished")
 }
