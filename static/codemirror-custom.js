a72a300bca075eb29092169495a88deb5d946a88
John Schroeder <john@schroederspace.com>
2019-01-22 10:37:54 -0500

    add syntax toggling

--- static/codemirror-custom.js
+++ static/codemirror-custom.js
@@ -1,8 +1,29 @@
+
 // Initialize the codemirror editor
-var editor = CodeMirror.fromTextArea(document.getElementById("body"), {
-    mode: 'markdown',
-    lineNumbers: false,
-    theme: "default",
-    fencedCodeBlockHighlighting: true,
-    lineWrapping: true
+var editor = null;
+
+function editorInit() {
+    return CodeMirror.fromTextArea(document.getElementById('body'), {
+        mode: 'markdown',
+        lineNumbers: false,
+        theme: "default",
+        fencedCodeBlockHighlighting: true,
+        lineWrapping: true
+    });
+}
+
+var togglebutton = document.getElementById('syntaxtoggle');
+togglebutton.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (!editor) {
+        editor = editorInit();
+        togglebutton.innerText = 'syntax off';
+    }
+    else {
+        editor.toTextArea();
+        editor = null;
+        togglebutton.innerText = 'syntax on';
+    }
 });
+
+editor = editorInit();
